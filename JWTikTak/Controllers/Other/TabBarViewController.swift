bab1fb87850c9d0d225bfb9172ff3b04231eee90
Amitai Blickstein <blickstein@gmail.com>
2022-02-11 11:35:45 -0500

    Basic TabBar setup

--- JWTikTak/Controllers/Other/TabBarViewController.swift
+++ JWTikTak/Controllers/Other/TabBarViewController.swift
@@ -6,24 +6,41 @@
 //
 
 import UIKit
+import SwiftUI
 
 class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupControllers()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupControllers() {
+        let homeVC          = HomeViewController()
+        let exploreVC       = ExploreViewController()
+        let cameraVC        = CameraViewController()
+        let notificationsVC = NotificationsViewController()
+        let profileVC       = ProfileViewController()
+        
+        homeVC.title          = "Home"
+        exploreVC.title       = "Explore"
+        // The camera VC does not needa title
+        notificationsVC.title = "Notifications"
+        profileVC.title       = "Profile"
+        
+        let homeNav          = UINavigationController(rootViewController: homeVC)
+        let exploreNav       = UINavigationController(rootViewController: exploreVC)
+        // The camera VC does not need a navigation controller
+        let notificationsNav = UINavigationController(rootViewController: notificationsVC)
+        let profileNav       = UINavigationController(rootViewController: profileVC)
+        
+        homeNav.tabBarItem          = UITabBarItem(title: nil, image:  UIImage(systemName: "house"), selectedImage: nil)
+        exploreNav.tabBarItem       = UITabBarItem(title: nil, image: UIImage(systemName: "magnifyingglass"), selectedImage: nil)
+        cameraVC.tabBarItem         = UITabBarItem(title: nil, image: UIImage(systemName: "camera"), selectedImage: nil)
+        notificationsNav.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "bell"), selectedImage: nil)
+        profileNav.tabBarItem       = UITabBarItem(title: nil, image: UIImage(systemName: "person.circle"), selectedImage: nil)
+        
+        setViewControllers([homeNav, exploreNav, cameraVC, notificationsNav, profileNav], animated: false)
     }
-    */
-
+    
 }
