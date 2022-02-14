c9f4c02ca3af7114eab0e851662263677f3ddccb
Amitai Blickstein <blickstein@gmail.com>
2022-02-13 19:35:04 -0500

    Adds scrollview

--- JWTikTak/Controllers/Core/HomeViewController.swift
+++ JWTikTak/Controllers/Core/HomeViewController.swift
@@ -9,12 +9,32 @@ import UIKit
 
 // Where the feed shows
 class HomeViewController: UIViewController {
+    private let hScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.bounces = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.backgroundColor = .systemRed
+        return scrollView
+    }()
+    
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        view.addSubview(hScrollView)
+        setupFeed()
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        hScrollView.frame = view.bounds
+    }
+    
+    /// Have one horizontal scroll view that can have two paging controllers
+    private func setupFeed() {
+        
+    }
 }
 
