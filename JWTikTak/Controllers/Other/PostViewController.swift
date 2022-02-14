beee97af1b8813706245eca721f7533843c980c9
Amitai Blickstein <blickstein@gmail.com>
2022-02-13 23:40:00 -0500

    Builds out trivial but functional PostModel and VC

--- JWTikTak/Controllers/Other/PostViewController.swift
+++ JWTikTak/Controllers/Other/PostViewController.swift
@@ -6,24 +6,25 @@
 //
 
 import UIKit
+import RandomColor
 
 class PostViewController: UITabBarController {
-
+    let model: PostModel
+    
+    init(model: PostModel) {
+        self.model = model
+        super.init(nibName: nil, bundle: nil)
+        view.backgroundColor = randomColor()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- JWTikTak/Models/PostModel.swift
+++ JWTikTak/Models/PostModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct PostModel: Equatable {
+    /// A unique identifier
+    let identifier: String
+    
+    // For debugging
+    static func mockModels() -> [PostModel] {
+        Array(0...100).compactMap({_ in
+            PostModel(identifier: UUID().uuidString)
+        })
+    }
+    
+    static func ==(lhs: PostModel, rhs: PostModel) -> Bool {
+        lhs.identifier == rhs.identifier
+    }
+}
+
