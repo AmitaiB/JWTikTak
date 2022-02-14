19b8d12e1a8a5b1e5bee11f3c649868884c786e9
Amitai Blickstein <blickstein@gmail.com>
2022-02-13 22:56:37 -0500

    Adds a the basic navigation scroll views
    A scrollview with paging enabled for vertical  functionality, with double width — two feeds side by side. HomeViewController—ForYou composed.
    
    Uses mocked up views, colors, placeholders for the Post views to come.
    
    Some optimization

--- JWTikTak/Controllers/Core/HomeViewController.swift
+++ JWTikTak/Controllers/Core/HomeViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class HomeViewController: UIViewController {
     private let hScrollView: UIScrollView = {
         let scrollView = UIScrollView()
-        scrollView.bounces = false
         scrollView.showsHorizontalScrollIndicator = false
+        scrollView.bounces         = false
+        scrollView.isPagingEnabled = true
         scrollView.backgroundColor = .systemRed
         return scrollView
     }()
@@ -24,7 +25,9 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         view.addSubview(hScrollView)
-        setupFeed()
+        setupFeeds()
+        // Starts the app off in the Right Feed (ForYou)
+        hScrollView.contentOffset = CGPoint(x: view.width, y: 0)
     }
 
     override func viewDidLayoutSubviews() {
@@ -33,8 +36,65 @@ class HomeViewController: UIViewController {
     }
     
     /// Have one horizontal scroll view that can have two paging controllers
-    private func setupFeed() {
+    private func setupFeeds() {
+        // This will allow paging 'left' and 'right' between the two page view controllers (of equal width, side by side).
+        hScrollView.contentSize = CGSize(width: view.width * 2, height: view.height)
         
+        [.following, .forYou]
+            .forEach { setupFeed(type: $0) }
+    }
+    
+    /// On the Right Side. 'Default' view.
+    let forYouPageViewController    = PageViewController()
+    /// On the Left Side.
+    let followingPageViewController = PageViewController()
+
+    enum FeedType { case forYou, following }
+    
+    private func setupFeed(type: FeedType) {
+        let pageViewController = type == .following ?
+        followingPageViewController : forYouPageViewController
+        
+        let mockVC = UIViewController()
+        mockVC.view.backgroundColor = .blue
+        
+        pageViewController.setViewControllers(
+            [mockVC],
+            direction: .forward,
+            animated: true,
+            completion: nil)
+        
+        pageViewController.dataSource = self
+        
+        hScrollView.addSubview(pageViewController.view)
+        // size is a standard device size, position is origin. SECOND feed ('right' side).
+        let xPos = type == .following ? 0 : view.width
+        pageViewController.view.frame = CGRect(x: xPos,
+                                               y: 0,
+                                               width: hScrollView.width,
+                                               height: hScrollView.height)
+        addChild(pageViewController)
+        pageViewController.didMove(toParent: self)
     }
 }
 
+extension HomeViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let randoVC = UIViewController()
+        randoVC.view.backgroundColor = [.red, .gray, .green].randomElement()
+        return randoVC
+    }
+}
+
+class PageViewController: UIPageViewController {
+    convenience init() {
+        self.init(
+            transitionStyle: .scroll,
+            navigationOrientation: .vertical,
+            options: [:])
+    }
+}
--- JWTikTak/Resources/Extensions/UIKit+Extensions.swift
+++ JWTikTak/Resources/Extensions/UIKit+Extensions.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import UIKit
+
+// To help with frame-based layouts.
+extension UIView {
+    var width: CGFloat  { frame.size.width }
+    var height: CGFloat { frame.size.height }
+    var left: CGFloat   { frame.origin.x }
+    var right: CGFloat  { left + width }
+    var top: CGFloat    { frame.origin.y }
+    var bottom: CGFloat { top + height }
+}
