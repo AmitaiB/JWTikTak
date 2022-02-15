4f1d2525ec5b71d24a00bb54d447e83298fe9e57
Amitai Blickstein <blickstein@gmail.com>
2022-02-15 16:53:05 -0500

    Adds reaction buttons to PostVC

--- JWTikTak/Controllers/Other/PostViewController.swift
+++ JWTikTak/Controllers/Other/PostViewController.swift
@@ -6,11 +6,24 @@
 //
 
 import UIKit
+import SnapKit
 import RandomColor
 
 class PostViewController: UITabBarController {
     let model: PostModel
+
+    private lazy var likeButton: UIButton = {
+       createButton(withSymbol: L10n.SFSymbol.likeFill)
+    }()
     
+    private lazy var commentButton: UIButton = {
+        createButton(withSymbol: L10n.SFSymbol.commentFill)
+    }()
+    
+    private lazy var shareButton: UIButton = {
+        createButton(withSymbol: L10n.SFSymbol.share)
+    }()
+
     init(model: PostModel) {
         self.model = model
         super.init(nibName: nil, bundle: nil)
@@ -20,11 +33,32 @@ class PostViewController: UITabBarController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let reactionButtons = [likeButton, commentButton, shareButton]
+        reactionButtons.forEach {
+            $0.snp.makeConstraints { $0.width.height.equalTo(40)}
+        }
+        
+        let buttonsRow = UIStackView(arrangedSubviews: reactionButtons)
+        view.addSubview(buttonsRow)
+        
+        buttonsRow.axis         = .vertical
+        buttonsRow.spacing      = 15
+        buttonsRow.alignment    = .center
+        buttonsRow.distribution = .fillEqually
+        buttonsRow.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-150)
+            make.right.equalToSuperview().offset(-10)
+        }
+    }
+    
+    private func createButton(withSymbol symbolName: String) -> UIButton {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: symbolName), for: .normal)
+        button.tintColor = .white
+        return button
     }
 }
