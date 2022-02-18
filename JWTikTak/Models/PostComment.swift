81696f324aa86e7256526842b1dd86458a966886
Amitai Blickstein <blickstein@gmail.com>
2022-02-17 22:56:44 -0500

    Adds User and PostComment models

---
+++ JWTikTak/Models/PostComment.swift
@@ -0,0 +1,14 @@
+//
+//  PostCommentModel.swift
+//  JWTikTak
+//
+//  Created by Amitai Blickstein on 2/17/22.
+//
+
+import Foundation
+
+struct PostComment {
+    let text: String
+    let user: User
+    let date: Date
+}
---
+++ JWTikTak/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  JWTikTak
+//
+//  Created by Amitai Blickstein on 2/17/22.
+//
+
+import Foundation
