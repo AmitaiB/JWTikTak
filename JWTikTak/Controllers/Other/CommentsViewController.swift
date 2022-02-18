438e4a3c14de069e9f0109fbd6765fb2d18f06c5
Amitai Blickstein <blickstein@gmail.com>
2022-02-17 22:59:26 -0500

    Adds comments tableview
    Also Post and User implementations

--- JWTikTak/Controllers/Other/CommentsViewController.swift
+++ JWTikTak/Controllers/Other/CommentsViewController.swift
@@ -6,9 +6,18 @@
 //
 
 import UIKit
+import SnapKit
 
 class CommentsViewController: UIViewController {
     private let post: PostModel
+    private var comments = [PostComment]()
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "cell")
+        return tableView
+    }()
     
     init(post: PostModel) {
         self.post = post
@@ -23,12 +32,36 @@ class CommentsViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .white
-        fetchPostComments()
-        
         view.layer.cornerRadius = 5
+
+        fetchPostComments()
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.snp.makeConstraints { make in
+            make.left.right.bottom.equalToSuperview()
+            make.top.equalToSuperview().offset(40)
+        }
+        tableView.backgroundColor = .systemGreen
     }
     
     private func fetchPostComments() {
-        
+        comments = PostComment.mockComments()
     }
+}
+
+extension CommentsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        comments.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = comments[indexPath.row].text
+        return cell
+    }
+}
+
+extension CommentsViewController: UITableViewDelegate {
+    
 }
--- JWTikTak/Models/PostComment.swift
+++ JWTikTak/Models/PostComment.swift
@@ -11,4 +11,16 @@ struct PostComment {
     let text: String
     let user: User
     let date: Date
+    
+    static func mockComments() -> [PostComment] {
+        let user = User(username: "oswaldfriend",
+                        profilePictureURL: nil,
+                        identifier: UUID().uuidString)
+        
+        return [
+            "Look at my amazing post!",
+            "Hey, its another post — ya boy!",
+            "I'm learning so much!",
+        ].map {PostComment(text: $0, user: user, date: Date())}
+    }
 }
