b2810d4c6a5e2362fe4685506ead7e0b1179cfde
Amitai Blickstein <blickstein@gmail.com>
2022-03-02 19:32:17 -0500

    Adds SignInViewController subviews (buttons, fields)

--- JWTikTak/Controllers/Other/Authentication/SignInViewController.swift
+++ JWTikTak/Controllers/Other/Authentication/SignInViewController.swift
@@ -6,28 +6,110 @@
 //
 
 import UIKit
+import Actions
+import SnapKit
 
 class SignInViewController: UIViewController {
     /// Allows the presenting view controller to respond to this view controller's
     /// dismissal, or otherwise presenting its function.
     public var viewControllerCompletion: (() -> Void)?
 
+    private let logoImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.contentMode         = .scaleAspectFit
+        imageView.image               = Asset.logo.image
+        imageView.layer.cornerRadius  = 10
+        imageView.layer.masksToBounds = true
+        
+        return imageView
+    }()
+    
+    private let emailField    = AuthField(type: .email)
+    private let passwordField = AuthField(type: .password)
+    
+    private let forgotPasswordButton = AuthButton(type: .plain, title: "Forgot Password?")
+    private let signInButton = AuthButton(type: .signIn, title: nil)
+    private let signUpButton = AuthButton(type: .plain, title: "New user? Create account!")
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemBackground
         title = "Sign In"
+        view.backgroundColor = .systemBackground
+        view.addSubviews([
+            logoImageView,
+            emailField,
+            passwordField,
+            signInButton,
+            signUpButton,
+            forgotPasswordButton
+        ])
+        
+        emailField.delegate    = self
+        passwordField.delegate = self
+        
+        configureButtons()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let interViewSpace = 20
+        
+        
+        logoImageView.snp.makeConstraints { make in
+            make.height.width.equalTo(UIScreen.main.bounds.width / 4)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaInsets.top + interViewSpace)
+        }
+        
+        emailField.snp.makeConstraints { make in
+            make.top.equalTo(logoImageView.snp.bottom).offset(interViewSpace)
+            make.centerX.equalToSuperview()
+            make.width.equalToSuperview().offset(-interViewSpace)
+            make.height.equalTo(60)
+        }
+        
+        passwordField.snp.makeConstraints { make in
+            make.top.equalTo(emailField.snp.bottom).offset(interViewSpace)
+            make.width.height.centerX.equalTo(emailField)
+        }
+        
+        signInButton.snp.makeConstraints { make in
+            make.top.equalTo(passwordField.snp.bottom).offset(interViewSpace)
+            make.width.height.centerX.equalTo(passwordField)
+        }
+        
+        forgotPasswordButton.snp.makeConstraints { make in
+            make.top.equalTo(signInButton.snp.bottom).offset(interViewSpace)
+            make.width.height.centerX.equalTo(signInButton)
+        }
+        
+        signUpButton.snp.makeConstraints { make in
+            make.top.equalTo(forgotPasswordButton.snp.bottom).offset(interViewSpace)
+            make.width.height.centerX.equalTo(forgotPasswordButton)
+        }
     }
-    */
+    
+    
+    private func configureButtons() {
+        
+        
+        signInButton.add(event: .touchUpInside) {
+            
+        }
+        
+        signUpButton.add(event: .touchUpInside) {
+            
+        }
+        
+        forgotPasswordButton.add(event: .touchUpInside) {
+            
+        }
+    }
+}
 
+extension SignInViewController: UITextFieldDelegate {
+    
 }
