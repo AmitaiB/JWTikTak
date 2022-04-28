2ea062aa9b35c48b9a1061ac1fbefbf3dd10e508
Amitai Blickstein <blickstein@gmail.com>
2022-04-28 14:45:31 -0400

    Implements CaptionViewController's post video behavior

--- JWTikTak/Controllers/Other/CaptionViewController.swift
+++ JWTikTak/Controllers/Other/CaptionViewController.swift
@@ -6,24 +6,66 @@
 //
 
 import UIKit
+import SCLAlertView
 
 class CaptionViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    let videoURL: URL
+    
+    init(videoURL: URL) {
+        self.videoURL = videoURL
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Add Caption"
+        view.backgroundColor = .systemBackground
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: L10n.post,
+            style: .done,
+            action: { [weak self] in self?.postVideo() }
+        )
+    }
 
+    private func postVideo() {
+        // TODO: post video here!
+        // Generate a unique video name based on id
+        let newVideoName = StorageManager.shared.generateVideoName()
+        
+        // upload video
+        StorageManager.shared.uploadVideoURL(from: videoURL, fileName: newVideoName) {
+            switch $0 {
+                case .success(_):
+                    // update db
+                    DatabaseManager.shared.insertPost(filename: newVideoName) { [weak self] result in
+                        switch result {
+                            case .success(_):
+                                self?.handlePostInsertionSuccess()
+                            case .failure(let error):
+                                self?.handleOpError(error)
+                        }
+                    }
+                case .failure(let error):
+                    self.handleOpError(error)
+            }
+        }
+    }
+    
+    private func handlePostInsertionSuccess() {
+        // reset camera, and switch to feed
+        navigationController?.popToRootViewController(animated: true)
+        tabBarController?.selectedIndex = 0
+        tabBarController?.tabBar.isHidden = false
+        SCLAlertView().showSuccess(L10n.success)
+    }
+    
+    private func handleOpError(_ error: Error) {
+        // alert
+        SCLAlertView().showError(L10n.error, subTitle: error.localizedDescription)
+    }
 }
