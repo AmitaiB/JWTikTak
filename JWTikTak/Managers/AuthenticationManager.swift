fa3e66783d1543204e3fd198866a39dfa66ace85
Amitai Blickstein <blickstein@gmail.com>
2022-04-28 14:42:23 -0400

    Adds username property to AuthenticationManager
    Also refactors out nested code.

--- JWTikTak/Managers/AuthenticationManager.swift
+++ JWTikTak/Managers/AuthenticationManager.swift
@@ -17,6 +17,8 @@ final class AuthManager {
     public static let shared = AuthManager()
     private init() {}
     
+    private weak var database = DatabaseManager.shared
+    
     enum SignInMethod {
         case email
         case google
@@ -30,6 +32,7 @@ final class AuthManager {
     
     // Public
     public var isSignedIn: Bool { Auth.auth().currentUser != nil }
+    public var currentUsername: String? { _currentUsername }
     
     /// Signs in using an email address and password.
     public func signIn(withEmail email: String,
@@ -37,22 +40,29 @@ final class AuthManager {
                        completion: @escaping AuthEmailResultCompletion) {
         Auth.auth().signIn(withEmail: email, password: password) { result, error in
             switch (result, error) {
-                    // Happy path, successful sign in.
-                case (.some, .none):
-                    completion(.success(email))
                     // Firebase reported an error.
                 case (_, .some(let firebaseError)):
                     completion(.failure(firebaseError))
+
                     // No firebase response = app-level error
                 case (.none, _):
                     completion(.failure(AuthError.signInFailed))
+
+                    // Happy path, successful sign in —> also save username in memory
+                case (.some, .none):
+                    completion(.success(email))
+                    self.database?.getUsername(
+                        for: email,
+                        completion: self.handleUsernameFetch
+                    )
             }
         }
     }
-    
+        
     public func signOut(completion: (Bool) -> Void) {
         do {
             try Auth.auth().signOut()
+            _currentUsername = nil
             completion(true)
         }
         catch {
@@ -77,6 +87,19 @@ final class AuthManager {
         
     }
     
+    
+    // MARK: - Helper methods
+    
+    private let handleUsernameFetch: (Result<String, Error>) -> Void = {
+        switch $0 {
+            case .success(let username):
+                _currentUsername = username
+                print("Got username: \(username)")
+            case .failure(let error):
+                print(error.localizedDescription)
+        }
+    }
+    
     private func handleUserCreation(
         forUsername username: String,
         email: String,
@@ -89,24 +112,45 @@ final class AuthManager {
                 // TODO: Check for username availability before attempting creation?
             case (_, .some(let firebaseError)):
                 completion(.failure(firebaseError))
+
                 // No firebase response = app-level error
             case (.none, _):
                 completion(.failure(AuthError.userCreationFailed))
-                // Happy path, if successful (user creation + sign in) -> register user in db.
+
+                // Happy path, if successful (user creation + sign in) -> register user in db
+                // and save the username locally
             case (.some, .none):
-                DatabaseManager.shared.insertUser(
-                    withEmail: email,
+                self.handleSuccessfulUserCreationAttempt(
+                    email: email,
                     username: username,
-                    completion: { dbResult in
-                        switch dbResult {
-                            case .success(_):
-                            // discard the dbRef, report success if no Auth or Firebase errors
-                                completion(.success(email))
-                            case .failure(let dbEerror):
-                                completion(.failure(dbEerror))
-                        }
-                    }
+                    completion: completion
                 )
         }
     }
+    
+    private func handleSuccessfulUserCreationAttempt(email: String,
+                                 username: String,
+                                 completion: @escaping AuthEmailResultCompletion) {
+        database?.insertUser(
+            withEmail: email,
+            username: username,
+            completion: { dbResult in
+                switch dbResult {
+                    case .success(_):
+                        // discard the dbRef, report success if no Auth or Firebase errors
+                        completion(.success(email))
+                        // and save the username locally
+                        _currentUsername = username
+                    case .failure(let dbEerror):
+                        completion(.failure(dbEerror))
+                }
+            }
+        )
+    }
+}
+
+// Workaround to allow for cleaner code blocks in this file.
+fileprivate var _currentUsername: String? {
+    set { UserDefaults.standard.set(newValue, forKey: L10n.Key.username) }
+    get { UserDefaults.standard.string(forKey: L10n.Key.username) }
 }
